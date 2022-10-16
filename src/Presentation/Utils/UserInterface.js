134cc5b408f45a46c7252e2ea86812d04980a2b9
Szymon Orchowski <szymonorchowski@gmail.com>
2022-10-16 15:05:48 +0100

    Show Details button link same tab

--- src/Presentation/Utils/UserInterface.js
+++ src/Presentation/Utils/UserInterface.js
@@ -14,7 +14,7 @@ export default class UserInterface extends EventEmitter
     {
         const detailedScreenLink = document.createElement('button')
         detailedScreenLink.setAttribute('id', 'showDetails-button')
-        detailedScreenLink.onclick = () => {window.open(`http://ec2-34-244-143-219.eu-west-1.compute.amazonaws.com:8080/dashboards/edit/#96054cad-6e19-2b84-5c3f-b0c5ef803ae1?viewstate={%22selected_unit%22:%22${serial}%22}`)}
+        detailedScreenLink.onclick = () => {window.open(`http://ec2-34-244-143-219.eu-west-1.compute.amazonaws.com:8080/dashboards/#96054cad-6e19-2b84-5c3f-b0c5ef803ae1?viewstate={%22selected_unit%22:%22${serial}%22}`,"_self")}
         detailedScreenLink.innerHTML = 'Show Details'
         document.body.appendChild(detailedScreenLink)
     }
