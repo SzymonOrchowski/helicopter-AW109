d7fd5ab2b5ec4237707af95ecb835cee86044f71
SzymonOrchowski <szymonorchowski@gmail.com>
2023-03-24 12:37:30 +0000

    Link IP changed after Dev server reboot

--- src/Presentation/Utils/UserInterface.js
+++ src/Presentation/Utils/UserInterface.js
@@ -15,7 +15,7 @@ export default class UserInterface extends EventEmitter
     {
         const detailedScreenLink = document.createElement('button')
         detailedScreenLink.setAttribute('id', 'showDetails-button')
-        detailedScreenLink.onclick = () => {window.open(`http://ec2-34-244-143-219.eu-west-1.compute.amazonaws.com:8080/dashboards/#96054cad-6e19-2b84-5c3f-b0c5ef803ae1?viewstate={%22selected_unit%22:%22${serial}%22}`, "_top")}
+        detailedScreenLink.onclick = () => {window.open(`http://ec2-54-77-135-6.eu-west-1.compute.amazonaws.com:8080/dashboards/#96054cad-6e19-2b84-5c3f-b0c5ef803ae1?viewstate={%22selected_unit%22:%22${serial}%22}`, "_top")}
         detailedScreenLink.innerHTML = 'Show Details'
         document.body.appendChild(detailedScreenLink)
     }
